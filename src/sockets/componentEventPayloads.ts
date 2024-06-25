0abf9c3aa453d6574693df1d8675dad20ce36423
Nathan Oran <noran@deeplocal.com>
2024-06-25 13:20:57 +0000

    Merged in GUM-1583/component-connected-payload-updates (pull request #16)
    [GUM-1583] update Component Connected Payload to match discussions with E, and also match hardware db column name for connectedChangedAt
    
    * [GUM-1583] update Component Connected Payload to match discussions with E, and also match hardware db column name for connectedChangedAt
    
    
    Approved-by: Eric Chmielewski

--- src/sockets/componentEventPayloads.ts
+++ src/sockets/componentEventPayloads.ts
@@ -10,6 +10,13 @@ export type ExhibitOnlinePayload = {
     lastOnlineChange: string; // ISO 8601 timestamp
 };
 
+export const COMPONENT_CATEGORY = {
+    SOFTWARE: 'software',
+    HARDWARE: 'hardware',
+} as const;
+
+export type ComponentCategory = ObjectValues<typeof COMPONENT_CATEGORY>;
+
 export const COMPONENT_TYPE = {
     CUSTOM_APPLICATION: 'custom-application',
     OS_MONITOR: 'os-monitor',
@@ -21,8 +28,8 @@ export type ComponentType = ObjectValues<typeof COMPONENT_TYPE>;
 
 export type ComponentConnectedPayload = {
     componentId: string | number; // uuid or number
-    componentType: ComponentType;
-    lastConnectedChange: string; // ISO 8601 timestamp
+    category: ComponentCategory;
+    connectedChangedAt: string; // ISO 8601 timestamp
 };
 
 export type ComponentDisconnectedPayload = ComponentConnectedPayload;
