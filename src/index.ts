28d4b475d7254323fd45a188f7a353087e277de4
Kyle Bennett <kbennett@deeplocal.com>
2024-07-29 16:01:56 -0400

    remove dotenv, didn't actually want it

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,3 @@ export * from './sockets';
 export * from './types';
 export * from './utils';
 
-import dotenv from 'dotenv';
-import path from 'path';
-
-// Load the .env file
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
