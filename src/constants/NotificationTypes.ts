392c7ca9b96c762fb341ffb28bb3634e321bc659
Harrison Callahan <34285101+bracketbear@users.noreply.github.com>
2024-10-23 12:27:48 -0700

    Update NotificationTypes.ts

--- src/constants/NotificationTypes.ts
+++ src/constants/NotificationTypes.ts
@@ -3,6 +3,7 @@ export const NOTIFICATION_TYPES = {
     notify_op_mode: 'notify_op_mode',
     notify_connected: 'notify_connected',
     notify_error_logs: 'notify_error_logs',
+    notify_exhibit_health: 'notify_exhibit_health',
     notify_on_delete: 'notify_on_delete',
     notify_scene_changes: 'notify_scene_changes',
     notify_hardware_change: 'notify_hardware_change',
