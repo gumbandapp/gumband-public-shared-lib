6fc8e8c4354786f747d8cef342c746c8c6045135
Harrison Callahan <34285101+bracketbear@users.noreply.github.com>
2024-11-05 15:44:37 -0800

    Reverts usefulTS changes.

--- src/utils/usefulTS.ts
+++ src/utils/usefulTS.ts
@@ -1,6 +1,10 @@
+/* eslint-disable no-process-env */
+// Disabling the use of prcoess env in this file because we use it to do validation
 import { LOG_LEVELS, LogLevelType } from '../constants/LogLevels';
 
+
 export type ObjectValues<T> = T[keyof T];
+
 
 /**
  * Typescript exhaustive guard (See: https://www.google.com/search?q=typescript+exhaustive+guard)
@@ -19,6 +23,22 @@ export const isNonEmptyString = (unknownString: unknown): unknownString is strin
     return typeof unknownString === 'string' && unknownString !== '';
 };
 
+/**
+ * Utility function that will eventually become part of a shared library
+ *
+ * @param {string} name - name of the environment variable to parse
+ * @return {string} value of the environment variable
+ * @throws {Error} when variable is not found in the environment
+ */
+export const parseEnvStrOrError = (name: string): string => {
+    const env = process.env[name] || null;
+
+    if (env == null) {
+        throw Error(`Environment variable [${name}] must be provided and was not`);
+    }
+
+    return env;
+};
 
 /**
  * @description a type guard that ensures an env variable is an acceptable log level (based on the npm log levels implemented by winston), useful for setting env vars
